af1d2c203faae370aae760a53c5afd0dfb047d0c
Maxlogin <Rustik@ru.com>
2023-04-24 13:44:41 +0300

    Готовое задание 1

--- Prakt__6/MainWindow.xaml.cs
+++ Prakt__6/MainWindow.xaml.cs
@@ -18,11 +18,28 @@ namespace Prakt__6
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    
+public partial class MainWindow : Window
     {
+        static int f(int x)
+        {
+            return x % 10;
+        }
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            int a = int.Parse(txtNumber1.Text);
+            int z;
+            int b = int.Parse(txtNumber2.Text);
+            z = f(a) + f(b);
+            lblResult.Content = $"Y={z:N3}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
