57910f6c665f4ea913753c8c0397968ae10f3cea
Maxlogin <Rustik@ru.com>
2023-04-24 14:03:17 +0300

    Готовое задание 2

---
+++ Zad_2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zad_2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double f(int n,double x)
+        {
+            return Math.Pow(x, n) / n;
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(txtNumber1.Text);
+            lblResult.Content = $"Y={f(2, x) + f(4, x) + f(6, x)}";
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
