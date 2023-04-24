a374b22d38f6874a96b79d27e8f6c601539b005e
Maxlogin <Rustik@ru.com>
2023-04-24 14:20:39 +0300

    Готовое задание 3

---
+++ ZAD3/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZAD3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                double radius = double.Parse(txtRadius.Text);
+                double area = CircleS(radius);
+                txtArea.Text = area.ToString();
+                statusText.Text = "";
+            }
+            catch (FormatException)
+            {
+                statusText.Text = "Ошибка: неверный формат ввода";
+            }
+            catch (Exception ex)
+            {
+                statusText.Text = $"Ошибка: {ex.Message}";
+            }
+        }
+
+            private double CircleS(double R)
+            {
+                const double PI = 3.14;
+                return PI * R * R;
+            }
+    }
+}
